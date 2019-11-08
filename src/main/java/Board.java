5d3f1c33a8c37c40d2d88a17c52f809ce60db986
Kenneth Truong <kennykpt@uw.edu>
2019-11-08 01:39:48 -0500

    Add zero-match criterion to board initialization

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -11,17 +11,38 @@ public class Board {
         populateBoard();
     }
 
+    // Populates the board by placing gems from left to right, top to bottom
     public void populateBoard() {
-        for (int i = 0; i < gems.length; i++)
-            for (int j = 0; j < gems[0].length; j++)
+        for (int i = 0; i < gems.length; i++) {
+            for (int j = 0; j < gems[0].length; j++) {
                 gems[i][j] = new Gem(i, j, GemType.getRandomType());
+                GemType current = gems[i][j].getType();
+
+                // Check left
+                if (j >= 2) {
+                    GemType left = gems[i][j - 1].getType();
+                    GemType leftTwo = gems[i][j - 2].getType();
+                    if (current == left && current == leftTwo) {
+                        gems[i][j] = new Gem(i, j, GemType.getRandomType());
+                        j--;
+                    }
+                }
+                // Check above
+                if (i >= 2) {
+                    GemType up = gems[i - 1][j].getType();
+                    GemType upTwo = gems[i - 2][j].getType();
+                    if (current == up && current == upTwo) {
+                        gems[i][j] = new Gem(i, j, GemType.getRandomType());
+                        j--;
+                    }
+                }
+            }
+        }
     }
 
     public void draw(GraphicsContext gc) {
-        for (int i = 0; i < gems.length; i++) {
-            for (int j = 0; j < gems[0].length; j++) {
+        for (int i = 0; i < gems.length; i++)
+            for (int j = 0; j < gems[0].length; j++)
                 gems[i][j].draw(gc);
-            }
-        }
     }
 }
